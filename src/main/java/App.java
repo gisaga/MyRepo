e0d238c4a7b11a51e2f12423206887d48345f81a
Vincnet Wu <549721298@qq.com>
2022-05-29 22:30:23 +0800

    init commit

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("a", "b");
+        System.out.println(strings);
+    }
+}
